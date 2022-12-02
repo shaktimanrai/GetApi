99ea98be27f0a319abb87adc0eb570b2609028cf
shaktimanrai <98007119+shaktimanrai@users.noreply.github.com>
2022-12-03 01:26:37 +0530

    GETApi

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import './App.css';
+import ComA from "./components/ComA"
+
+const App = () => {
+
+
+  return (
+    <>
+
+
+<ComA/>
+
+
+        
+     
+
+
+
+    </>
+  )
+}
+
+export default App
